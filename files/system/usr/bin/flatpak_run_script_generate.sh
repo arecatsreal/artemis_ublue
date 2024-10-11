94db2885b74fbde291aad101ea9b6feba63b3b8a
thecat <thecat@arecatsreal.com>
2024-10-11 11:50:17 -0500

    remove testing code

--- files/system/usr/bin/flatpak_run_script_generate.sh
+++ files/system/usr/bin/flatpak_run_script_generate.sh
@@ -4,5 +4,5 @@ if [ "$(whoami)" != "root" ]; then
   echo "MUST BE RUN AS ROOT!"
   exit 1
 else
-  flatpak list --app | awk 'NR > 1 {print "echo \"#!/bin/sh\nflatpak run " $2 " $@\" > ~/cats/" tolower($1) "\nchmod +x ~/cats/" tolower($1)}' | sh
+  flatpak list --app | awk 'NR > 1 {print "echo \"#!/bin/sh\nflatpak run " $2 " $@\" > /bin/" tolower($1) "\nchmod +x /bin/" tolower($1)}' | sh
 fi
